3495a09a50f970b3b145d6e98f63e777e86d8b7a
ketsueki33 <harsh.karaiya@gmail.com>
2025-01-29 12:33:45 +0530

    nvim/add gitsigns keymaps for navigating/staging/previewing hunks

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -16,14 +16,50 @@ return {
       topdelete = { text = '‾' },
       changedelete = { text = '~' },
     },
-    on_attach = function()
-      vim.keymap.set('n', '<leader>gbf', function()
-        require('gitsigns').blame()
-      end, { silent = true, noremap = true, desc = '[G]it [B]lame for [F]ile' })
 
-      vim.keymap.set('n', '<leader>gbl', function()
-        require('gitsigns').blame_line()
-      end, { silent = true, noremap = true, desc = '[G]it [B]lame for [L]ine' })
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation
+      map('n', ']c', function()
+        if vim.wo.diff then
+          return ']c'
+        end
+        vim.schedule(function()
+          gs.next_hunk { navigation_message = false }
+        end)
+        return '<Ignore>'
+      end, { expr = true, desc = 'Next hunk', silent = true })
+
+      map('n', '[c', function()
+        if vim.wo.diff then
+          return '[c'
+        end
+        vim.schedule(function()
+          gs.prev_hunk { navigation_message = false }
+        end)
+        return '<Ignore>'
+      end, { expr = true, desc = 'Previous hunk', silent = true })
+
+      -- Actions
+      map('n', '<leader>hs', gs.stage_hunk, { desc = 'stage/unstage hunk' })
+      map('n', '<leader>hp', gs.preview_hunk, { desc = 'Preview hunk' })
+      map('n', '<leader>gbf', gs.blame, { desc = '[G]it [B]lame for [F]ile' })
+      map('n', '<leader>gbl', gs.blame_line, { desc = '[G]it [B]lame for [L]ine' })
+      map('n', '<leader>gtb', gs.toggle_current_line_blame, { desc = 'Toggle current line blame' })
+
+      -- map('n', '<leader>hS', gs.stage_buffer)
+      -- map('n', '<leader>hR', gs.reset_buffer)
+      -- map('n', '<leader>hd', gs.diffthis)
+
+      -- Text object (for selecting hunks in visual mode)
+      map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'Select hunk' })
     end,
   },
 }
